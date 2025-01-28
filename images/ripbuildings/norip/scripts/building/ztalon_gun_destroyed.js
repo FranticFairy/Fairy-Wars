9303187a5dc01cba5ae0386dbdb33804f5928ee1
FranticFairy <fairyflossy42@gmail.com>
2025-01-28 17:28:53 +0100

    removed old stuff

--- images/ripbuildings/norip/scripts/building/ztalon_gun_destroyed.js
+++
@@ -1,5 +0,0 @@
-ZTALON_GUN_DESTROYED.loadSprites = function(building)
-{
-	building.loadSprite("talon+gun+destroyed", false);
-	building.loadSpriteV2("talon+gun+destroyed+mask", GameEnums.Recoloring_Table);
-};
